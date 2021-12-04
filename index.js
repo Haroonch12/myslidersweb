79c559dcc24e611969b826d8b3a93d821cc8ff22
Ch Haroon <chh10143@gmail.com>
2021-12-04 16:05:47 +0500

    design updated

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ $('.your-class').slick({
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToShow: 2,
+          slidesToScroll: 2
         }
       }
       // You can unslick at a given breakpoint now by adding:
